d8f2d0662c31d3ba264631e2d2c4f6163ba3bd0b
Larissa Cravo Carvalho Câmara Santos <153231533+L4r1ss4Cr4v0@users.noreply.github.com>
2024-08-21 15:12:34 -0300

    Update crewai-stocks.py

--- webappStocks/crewai-stocks.py
+++ webappStocks/crewai-stocks.py
@@ -1,10 +1,3 @@
-!pip install yfinance==0.2.41
-!pip install crewai==0.28.8
-!pip install 'crewai-tools'
-!pip install langchain==0.1.20
-!pip install langchain-openai==0.1.7
-!pip install langchain-community==0.0.38
-!pip install duckduckgo-search==5.3.0
 
 import json 
 import os
